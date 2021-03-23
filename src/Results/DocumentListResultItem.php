17146522781905004590cdcef3384367d2813bda
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-03-23 21:33:16 +0200

    More data access

--- src/Results/DocumentListResultItem.php
+++ src/Results/DocumentListResultItem.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Awanturist\NovaPoshtaAPI\Results;
 
 use Awanturist\NovaPoshtaAPI\Exceptions\NoSeatsAmountException;
+use Awanturist\NovaPoshtaAPI\Types\DocumentState;
 
 final class DocumentListResultItem
 {
@@ -31,5 +32,47 @@ final class DocumentListResultItem
         }
 
         return (int) $seats;
+    }
+
+    public function isPrinted(): bool
+    {
+        return (bool) $this->data['Printed'];
+    }
+
+    public function getWeight(): float
+    {
+        return (float) $this->data['Weight'];
+    }
+
+    public function getDocumentNumber(): string
+    {
+        return (string) $this->data['IntDocNumber'];
+    }
+
+    public function getShippingCost(): float
+    {
+        return (float) $this->data['CostOnSite'];
+    }
+
+    public function getAssessedCost(): float
+    {
+        return (float) $this->data['Cost'];
+    }
+
+    public function getState(): DocumentState
+    {
+        return new DocumentState((int) $this->data['StateId']);
+    }
+
+    public function getStateName(): string
+    {
+        return $this->data['StateName'];
+    }
+
+    public function getScanSheetNumber(): ?string
+    {
+        $number = $this->data['ScanSheetNumber'];
+
+        return $number ?: null;
     }
 }
---
+++ src/Types/DocumentState.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Awanturist\NovaPoshtaAPI\Types;
+
+use function in_array;
+
+final class DocumentState
+{
+    public const WAITING_FOR_RECEIPT = 1;
+    public const DELETED = 2;
+    public const NOT_FOUND = 3;
+    public const IN_THE_LOCALITY = 4;
+    public const IN_THE_LOCALITY_LOCAL_EXPRESS = 41;
+    public const ON_THE_WAY_TO = 5;
+    public const IN_THE_TARGET_LOCALITY = 6;
+    public const ARRIVED = [7, 8];
+    public const RECEIVED = 9;
+    public const RECEIVED_WAIT_FOR_PAYMENT = 10;
+    public const RECEIVED_BACKWARD_PAYMENT = 11;
+    public const COMPANY_PREPARING_SHIPPING = 12;
+    public const ON_THE_WAY_TO_ADDRESS = 101;
+    public const REFUSAL = [102, 103, 108];
+    public const ADDRESS_CHANGED = 104;
+    public const STOPPED_KEEPING = 105;
+    public const RECEIVED_AND_CREATE_BACKWARD_SHIPMENT = 106;
+    public const NOT_DELIVERED_NO_RECEIVER = 111;
+    public const DELIVERY_TIME_CHANGED = 112;
+    private int $state;
+
+    public function __construct(int $state)
+    {
+        $this->state = $state;
+    }
+
+    public function isReceived(): bool
+    {
+        return in_array($this->state, [
+            self::RECEIVED,
+            self::RECEIVED_WAIT_FOR_PAYMENT,
+            self::RECEIVED_BACKWARD_PAYMENT,
+            self::RECEIVED_AND_CREATE_BACKWARD_SHIPMENT,
+        ], true);
+    }
+}
