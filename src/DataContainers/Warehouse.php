e933d835d7ab65305a494aa73b3c8da7c84e5ff9
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-04-05 11:06:22 +0300

    Type convertion

--- src/DataContainers/Warehouse.php
+++ src/DataContainers/Warehouse.php
@@ -78,6 +78,6 @@ final class Warehouse extends DataContainer
 
     public function getNumber(): int
     {
-        return $this->data['Number'];
+        return (int) $this->data['Number'];
     }
 }
