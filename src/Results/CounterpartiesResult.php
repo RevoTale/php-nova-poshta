98b4ee2f41d139401c0828bf66f17a4dec360f59
Vlad Liakhovskyi <vladgadzyra@gmail.com>
2021-04-11 20:29:47 +0300

    Phpdoc

--- src/Results/CounterpartiesResult.php
+++ src/Results/CounterpartiesResult.php
@@ -8,6 +8,9 @@ use BladL\NovaPoshta\DataContainers\Counterparty;
 
 final class CounterpartiesResult extends Result
 {
+    /**
+     * @return Counterparty[]
+     */
     public function getCounterparties(): array
     {
         return array_map(static fn (array $data) => new Counterparty($data), $this->container->getData());
