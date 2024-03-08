add1c53d49a12315a0eb71fc9f012a215b521382
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-03-08 17:27:41 +0200

    More fields for settlement search item

--- src/DataAdapters/Entities/Location/SettlementItem.php
+++ src/DataAdapters/Entities/Location/SettlementItem.php
@@ -14,4 +14,68 @@ final readonly class SettlementItem extends Entity
     use RefTrait;
     use DescriptionWithRuTrait;
     use PartOfAreaTrait;
+
+    public function getArea(): string
+    {
+
+        return $this->getField('Area')->string();
+    }
+
+    public function getRegion(): ?string
+    {
+        $value = $this->getField('Region')->nullableScalar();
+        return $value === "" || $value === null ? null : (string)$value;
+    }
+
+    public function getRegionsDescription(): string
+    {
+        return $this->getField('RegionsDescription')->string();
+    }
+
+    public function getRegionsDescriptionRu(): string
+    {
+        return $this->getField('RegionsDescriptionRu')->string();
+    }
+
+    public function getRegionsDescriptionTranslit(): string
+    {
+        return $this->getField('RegionsDescriptionTranslit')->string();
+    }
+
+    public function getAreaDescription(): string
+    {
+        return $this->getField('AreaDescription')->string();
+    }
+
+    public function getAreaDescriptionRu(): string
+    {
+        return $this->getField('AreaDescription')->string();
+    }
+
+    public function getDescriptionTranslit(): string
+    {
+        return $this->getField('AreaDescription')->string();
+    }
+
+    public function getAreaDescriptionTranslit(): string
+    {
+        return $this->getField('AreaDescriptionTranslit')->string();
+    }
+
+    public function getSettlementTypeDescription(): string
+    {
+        return $this->getField('SettlementTypeDescription')->string();
+
+    }
+
+    public function getSettlementTypeDescriptionRu(): string
+    {
+
+        return $this->getField('SettlementTypeDescriptionRu')->string();
+    }
+
+    public function getSettlementTypeDescriptionTranslit(): string
+    {
+        return $this->getField('SettlementTypeDescriptionRu')->string();
+    }
 }
